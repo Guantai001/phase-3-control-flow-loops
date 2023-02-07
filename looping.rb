44de93ab03bcffc0585e66b499399f5741e7310d
guantai <jpguantai@gmail.com>
2023-02-07 23:12:35 +0300

    Completed Assignment

--- looping.rb
+++ looping.rb
@@ -1,11 +1,13 @@
 def happy_new_year
-  # your code here
-  10.downto(1) do |i|
+  # your code here use while loop
+  i = 10
+  while i > 0
     puts i
+    i -= 1
   end
   puts "Happy New Year!"
-end
 
+end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
